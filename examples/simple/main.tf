4c6fb6c85f38a896bda15ca842b7e74bc1ab4d68
Android Build Filesystem (ABFS) Team <no-reply@google.com>
2025-07-02 17:13:06 +0000

    - providing terraform.tf with backend example - non-local referencing of modules
    PiperOrigin-RevId: 778551543

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -23,7 +23,7 @@ moved {
 }
 
 module "abfs_server" {
-  source = "../../modules/server"
+  source = "github.com/terraform-google-modules/terraform-google-abfs//modules/server?ref=v0.5.0"
 
   project_id                   = data.google_project.project.project_id
   zone                         = var.zone
@@ -36,7 +36,7 @@ module "abfs_server" {
 }
 
 module "abfs_uploaders" {
-  source = "../../modules/uploaders"
+  source = "github.com/terraform-google-modules/terraform-google-abfs//modules/uploaders?ref=v0.5.0"
 
   project_id                           = data.google_project.project.project_id
   zone                                 = var.zone
