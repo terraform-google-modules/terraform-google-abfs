d406d62e12cb332e6745b4ab7a5a804c793123b1
Android Build Filesystem (ABFS) Team <no-reply@google.com>
2026-03-30 20:21:54 +0000

    CWS: support shared-VPC
    PiperOrigin-RevId: 891877550

--- blueprints/reference/variables.tf
+++ blueprints/reference/variables.tf
@@ -426,6 +426,7 @@ variable "cws_clusters" {
     network    = string
     region     = string
     subnetwork = string
+    vpc_project = optional(string)
     domain_config = optional(object({
       domain = string
     }))
